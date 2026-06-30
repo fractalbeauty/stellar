ac43d1a9e7a4c707faf3f9ac803139356256a70c
hazel <hazel@meows.zip>
2026-06-29 23:07:43 -0400

    tui: fix compilation

--- crates/stellar-tui/src/main.rs
+++ crates/stellar-tui/src/main.rs
@@ -1,39 +1,22 @@
 use std::time::Duration;
 
+use stellar::core::Core;
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
-    let (endpoint_id_tx, endpoint_id_rx) = tokio::sync::watch::channel(None);
-    let (devices_tx, devices_rx) = tokio::sync::watch::channel(Vec::new());
+    let core = Core::spawn().await.expect("Should spawn core");
 
-    let peers = stellar::sync::peers::PeersTask::spawn(devices_rx).unwrap();
-    let _ = endpoint_id_tx.send(Some(peers.endpoint_id()));
+    let verification_uri_complete = core
+        .start_device_code_flow()
+        .await
+        .expect("Should start device code flow");
 
-    let mut devices = stellar::sync::devices::Devices::new(endpoint_id_rx, devices_tx);
-
-    let device_code = devices.start_device_code_flow().await.unwrap();
     println!(
         "Visit the following URL to authorize the device: {}",
-        device_code.verification_uri_complete
+        verification_uri_complete
     );
-
-    devices.poll_device_code_flow(device_code).await.unwrap();
-    println!("Device authorized");
-
-    // let device_code = stellar::sync::devices::start_device_code_flow()
-    //     .await
-    //     .unwrap();
-    // println!(
-    //     "Visit the following URL to authorize the device: {}",
-    //     device_code.verification_uri_complete
-    // );
-    // let access_token = stellar::sync::devices::poll_device_code_flow(device_code)
-    //     .await
-    //     .unwrap();
-    // println!("Access token: {}", access_token);
-    // let sessions = stellar::sync::list_sessions(access_token).await.unwrap();
-    // println!("Sessions: {}", sessions);
 
     loop {
         tokio::time::sleep(Duration::from_secs(1)).await;
