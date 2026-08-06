6360498eae511585982beba24db00f4b5c002982
hazel <hazel@meows.zip>
2026-08-05 22:57:28 -0400

    tui: add profile args

--- crates/stellar-tui/src/main.rs
+++ crates/stellar-tui/src/main.rs
@@ -1,13 +1,43 @@
+use clap::{Args, Parser};
 use std::time::Duration;
 use stellar::core::Core;
+
+/// Stellar TUI
+#[derive(Parser)]
+#[command(about)]
+struct Cli {
+    #[command(flatten)]
+    profile: Profile,
+}
+
+#[derive(Args)]
+#[group(required = false, multiple = false)]
+struct Profile {
+    #[arg(long, short)]
+    profile: Option<String>,
+
+    #[arg(long, short)]
+    anonymous: bool,
+}
 
 #[tokio::main]
 async fn main() {
     // tracing_subscriber::fmt::init();
 
-    let core = Core::spawn("default".to_string())
-        .await
-        .expect("Should spawn core");
+    let cli = Cli::parse();
+
+    let profile = if let Some(profile) = cli.profile.profile {
+        profile
+    } else if cli.profile.anonymous {
+        let id: String = std::iter::repeat_with(fastrand::lowercase)
+            .take(7)
+            .collect();
+        format!("anonymous-{id}")
+    } else {
+        "default".to_string()
+    };
+
+    let core = Core::spawn(profile).await.expect("Should spawn core");
 
     if std::env::var("STELLAR_ADD_ENTITY").is_ok_and(|var| !var.is_empty()) {
         core.add_random_entity().unwrap();
