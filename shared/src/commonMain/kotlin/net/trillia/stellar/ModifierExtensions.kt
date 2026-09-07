1ab2e9cbf270c670143d7b68ae1e32b66f1e5a94
hazel <hazel@meows.zip>
2026-09-07 10:43:39 -0400

    app: add min drag distance to column dragging

--- shared/src/commonMain/kotlin/net/trillia/stellar/ModifierExtensions.kt
+++ shared/src/commonMain/kotlin/net/trillia/stellar/ModifierExtensions.kt
@@ -1,25 +1,66 @@
 package net.trillia.stellar
 
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.awaitEachGesture
+import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.changedToUp
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.positionChange
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
+/**
+ * Minimum drag distance before starting a drag gesture.
+ */
+private val minDragDistance: Dp = 8.dp
+
+/**
+ * Detects a horizontal drag gesture or tap gesture, with a minimum drag distance before
+ * starting a drag gesture.
+ */
 fun Modifier.pointerInputHorizontalDrag(
     key: String,
+    onTap: () -> Unit = {},
     onDragStart: () -> Unit,
     onDragEnd: () -> Unit,
     onDrag: (deltaX: Float) -> Unit,
 ): Modifier =
     this.then(
         Modifier.pointerInput(key) {
-            detectDragGestures(
-                onDragStart = { onDragStart() },
-                onDragEnd = { onDragEnd() },
-                onDragCancel = { onDragEnd() },
-                onDrag = { change, dragAmount ->
-                    change.consume()
-                    onDrag(dragAmount.x)
-                },
-            )
+            val minDragDistancePx = minDragDistance.toPx()
+
+            awaitEachGesture {
+                val down = awaitFirstDown(requireUnconsumed = false)
+                
+                var dragOffset = Offset.Zero
+                var dragStarted = false
+
+                while (true) {
+                    val event = awaitPointerEvent()
+                    val change = event.changes.firstOrNull { it.id == down.id } ?: break
+
+                    if (change.changedToUp()) {
+                        if (dragStarted) onDragEnd() else onTap()
+                        break
+                    }
+
+                    val delta = change.positionChange()
+                    if (delta != Offset.Zero) {
+                        if (dragStarted) {
+                            change.consume()
+                            onDrag(delta.x)
+                        } else {
+                            dragOffset += delta
+                            if (dragOffset.getDistance() >= minDragDistancePx) {
+                                dragStarted = true
+                                change.consume()
+                                onDragStart()
+                                onDrag(dragOffset.x)
+                            }
+                        }
+                    }
+                }
+            }
         },
     )
--- shared/src/commonMain/kotlin/net/trillia/stellar/desktop/table/TableHeaderColumn.kt
+++ shared/src/commonMain/kotlin/net/trillia/stellar/desktop/table/TableHeaderColumn.kt
@@ -1,7 +1,6 @@
 package net.trillia.stellar.desktop.table
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -14,7 +13,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontFamily
@@ -84,10 +82,9 @@ fun TableHeaderColumn(
                     .height(tableRowHeight)
                     .runIf(DEBUG_REORDER_DRAG) {
                         background(Color.Blue.copy(alpha = 0.5f))
-                    }.pointerInput(id) {
-                        detectTapGestures(onTap = { onTap() })
                     }.pointerInputHorizontalDrag(
                         key = id,
+                        onTap = onTap,
                         onDragStart = { columnState.handleReorderDragStart(id) },
                         onDragEnd = { columnState.handleReorderDragEnd(id, scope) },
                         onDrag = { deltaX -> columnState.handleReorderDragMove(id, deltaX, scope) },
